d8b2c54fb1abb0754807117a26479a48a3da6622
Alessandro Buonocore <60114301+bncal@users.noreply.github.com>
2023-02-25 23:22:16 +0100

    prisma schema + interfaces
    addes old prisma schema (users + posts) and old interfaces

---
+++ lib/enumerators.ts
@@ -0,0 +1,14 @@
+export enum LoginInfo {
+    OK = "You have logged in as ",
+    ERR = "Incorrect username or password"
+}
+
+export enum RegistrationInfo {
+    OK = "You have registered as ",
+    ERR = "Username or email already exists"
+}
+
+export enum HTTPCodes {
+    OK = 200,
+    ERR = 400
+}
