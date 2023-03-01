26fc45666d0857fd0db42778c7215e1126da6d33
Alessandro Buonocore <60114301+bncal@users.noreply.github.com>
2023-03-01 21:36:18 +0100

    preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         "./app/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        'node_modules/preline/dist/*.js',
     ],
     theme: {
         borderRadius: {
@@ -34,7 +35,7 @@ module.exports = {
     },
 
     plugins: [
-        require("daisyui"),
+        require('preline/plugin'),
 
     ],
 }
